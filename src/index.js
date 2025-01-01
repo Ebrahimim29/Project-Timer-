66e59eb1ed89151c4eea661ea723ea91ea0db39d
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-01-01 07:28:04 +0330

    New

--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-
-
-const tick=()=>{
-    const elem=(
-        <div>
-            <h1>سلام دوست من</h1>
-            <h2>
-                it is {new Date().toLocaleTimeString()}
+import "./Style.css"
+class Timer extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            time:new Date().toLocaleTimeString()
+        }
+    }
+    render(){
+        setInterval(() => {
+            this.setState({
+                time:new Date().toLocaleTimeString()
+            })            
+        }, 1000);
+        return(
+            <h2 className='Timer'>
+                it is {this.state.time}
             </h2>
-        </div>
-    )
-    createRoot(document.getElementById('root')).render(elem);
-   
+        )
+
+    }
 }
-setInterval(()=>{
-    tick();
-},1000)
+
+class Hello extends React.Component{
+    render(){
+        return(
+            <h1>سلام دوست من</h1>
+        )
+    }
+}
+class App extends React.Component{
+    render(){
+        return (
+        <div className='main'>
+            <Hello/>
+            <Timer/>
+        </div>
+        )
+    }
+}
+
+//let elem=new App
+
+// const tick=()=>{
+    createRoot(document.getElementById('root')).render(<App/>);   
+// }
+// setInterval(()=>{
+//     tick();
+// },1000)
 
 
 
